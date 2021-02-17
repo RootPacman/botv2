04b203da2a021fe9f26228b858537ef1ecb71ce7
RootPacman <75959846+RootPacman@users.noreply.github.com>
2021-02-17 19:35:44 +0700

    Add files via upload

---
+++ botv2.py
@@ -0,0 +1,44 @@
+import os
+import time
+from bs4 import BeautifulSoup
+import requests
+import random
+#Pacman
+os.system('clear')
+def U():
+#Gan
+	print("wait")
+	os.system('python -m pip install googlesearch-python')
+	print("")
+	os.system('clear')
+	print("""
+             ,----------------,              ,---------,
+        ,-----------------------,          ,"        ,"|
+      ," Pc Punya Pacman      ,"|        ,"        ,"  |
+     +-----------------------+  |      ,"        ,"    |
+     |  .-----------------.  |  |     +---------+      |
+     |  |   RootPacman    |  |  |     | -==----'|      |
+     |  | Black Attacker  |  |  |     |         |      |
+     |  |    Dark Sec     |  |  |/----|`---=    |      |
+     |  |   Dorking py    |  |  |   ,/|==== ooo |      ;
+     |  | +628570737087   |  |  |  // |(((( [33]|    ,"
+     |  `-----------------'  |," .;'| |((((     |  ,"
+     +-----------------------+  ;;  | |         |,"
+        /_)______________(_/  //'   | +---------+
+   ___________________________/___  `,
+  /  oooooooooooooooo  .o.  oooo /,   \,"-----------
+ / ==ooooooooooooooo==.o.  ooo= //   ,`\--{)B     ,"
+/_==__==========__==_ooo__ooo=_/'   /___________,"
+`-----------------------------'
+""")
+	print("")
+	print("Tools tidak di rekomendasikan untuk lonte cybel")
+	print("anak Terror Women / 10 Cute Girl Ga Boleh pake tools ini")
+	print('Recode?Izin +6285707370877,ga recode,titid/memek lo jadi busuk')
+	print('Note: cuma 100web yg ke ambil,biar tools nya ga error')
+U()
+from googlesearch import search
+query = str(input("Dork Nya Ngentot->: "))
+os.system('clear')
+for URL in search(query):
+  print(URL)
